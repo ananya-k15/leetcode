0a588cb203d5a2bd700ad8210ba58fd71789aa37
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2022-11-28 16:19:30 -0500

    Time: 154 ms (21.91%), Space: 15.2 MB (25.22%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,18 +3,13 @@ class Solution:
         x = len(prices)
         buy = 0
         sell = 1
-        profit = 0
         totalprofit = 0
         while(sell < x) : 
             diff = prices[sell] - prices[buy]
-            # print(buy, sell)
             if diff >= 0 : 
-                sell += 1
                 while (sell < x) : 
-                    if prices[sell] > prices[sell - 1] :
-                        sell += 1
-                    else : 
-                        break
+                    if prices[sell] > prices[sell - 1] : sell += 1
+                    else : break
                 sell -= 1
                 totalprofit += (prices[sell] - prices[buy])
                 buy = sell + 1
@@ -22,6 +17,5 @@ class Solution:
             elif diff < 0 : 
                 buy += 1
                 sell = buy + 1 
-            # profit += [j - prices[i] for j in prices[i+1:] if j > prices[i]]
         return totalprofit
         
