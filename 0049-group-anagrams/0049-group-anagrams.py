785a6d26d96281e1feee824371cfa1681449de8d
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2023-01-03 18:25:32 +0530

    Time: 133 ms (60.94%), Space: 18.2 MB (49.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = dict()
         for i in range(0, len(strs), 1) : 
-            alpha = ''.join(sorted(strs[i]))
+            alpha = tuple(sorted(strs[i]))
             anagrams[alpha] = [strs[i]] + anagrams.get(alpha, [])
         return anagrams.values()
         
