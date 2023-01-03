aba95cd35bdcc59e18b5a0edcacbb989b56fa30f
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2023-01-03 17:41:42 +0530

    Time: 116 ms (66.36%), Space: 15.3 MB (14.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,18 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) == 2 : 
             return [0, 1]
-        for i in range(len(nums)) :
+        # for i in range(len(nums)) :
+        #     left = target - nums[i]
+        #     # for j in range(i + 1, len(nums), 1) : 
+        #     #     if nums[j] == left : return [i, j]
+        #     if left in nums[i+1:] :
+        #         return [i, i + 1 + nums[i+1:].index(left)]
+        dnums = dict()
+        for i in range(len(nums)) : 
             left = target - nums[i]
-            # for j in range(i + 1, len(nums), 1) : 
-            #     if nums[j] == left : return [i, j]
-            if left in nums[i+1:] :
-                return [i, i + 1 + nums[i+1:].index(left)]
+            if left in dnums.keys() : 
+                return [i, dnums.get(left)]
+            else : 
+                dnums[nums[i]] = i
         
         
