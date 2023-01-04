6f696f45efe8e871e8500b662dba8e16c3ea5374
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2023-01-04 22:40:39 +0530

    Time: 310 ms (9.37%), Space: 24.6 MB (5.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,7 +3,16 @@ class Solution:
         freq = dict()
         for i in nums : 
             freq[i] = 1 + freq.get(i, 0)  
-        topk = [l[0] for l in sorted(freq.items(), 
-                                     key=lambda x:x[1], 
-                                     reverse=True)]
-        return topk[:k]       
+        count = {i:[] for i in range(1, len(nums) + 1, 1)}
+        for key, value in freq.items() : 
+            count[value].append(key)
+        topk = []; ind = len(nums); dupk = k
+        while dupk != 0 :
+            topk += count[ind]
+            dupk -= len(count[ind])
+            ind -= 1
+        return topk[:k] 
+    
+        # topk = [l[0] for l in sorted(freq.items(), 
+        #                              key=lambda x:x[1], 
+        #                              reverse=True)]
