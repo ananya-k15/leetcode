2e36821b9b4f22e2a3feaf8bdc1afc685a6714a2
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2023-01-06 16:08:13 +0530

    Time: 40 ms (96.20%), Space: 15 MB (33.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,4 +3,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if len(s) == 1 : return True
         news = ''.join(l for l in s if l.isalnum()).lower()
-        return news == news[::-1] 
+        w = len(news) // 2
+        if len(news) % 2 == 0 :
+            return news[:w] == news[:w-1:-1]
+        else : 
+            return news[:w] == news[:w:-1]
+        # return news == news[::-1] 
