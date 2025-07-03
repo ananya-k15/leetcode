ed66a53f6ee391f7c1e567602640a4d3dcc975ea
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2025-07-02 23:25:48 -0400

    Time: 15 ms (42.05%) | Memory: 17.7 MB (86.10%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters = dict()
+
+        for ch in s:
+            letters[ch] = letters.get(ch, 0) + 1
+        
+        for ch in t:
+            if ch in letters.keys():
+                if letters[ch] == 1:
+                    del letters[ch]
+                else:
+                    letters[ch] -= 1
+            else:
+                return False
+        
+        if len(letters) == 0:
+            return True
+        return False
