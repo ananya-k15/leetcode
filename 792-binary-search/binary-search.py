1c7b740df9349e9f9426f6b80c4ab022444e376f
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-01-13 10:59:37 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (9.26%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        def search_index(nums, left, right, target):
+            # check whether there are any elements in the provided range
+            if right - left == 1:
+                return left if nums[left] == target else -1
+            elif right <= left:
+                return -1
+            # if yes, compute middle index
+            else:
+                m = (left + right) // 2
+                if target == nums[m]:
+                    return m
+                elif target < nums[m]:
+                    return search_index(nums, left, m, target)
+                else:
+                    return search_index(nums, m+1, right, target)
+        
+        return search_index(nums, 0, len(nums), target)
+            
