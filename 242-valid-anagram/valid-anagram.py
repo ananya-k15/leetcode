ee848c2fcc715250d1e011681f611eece8356818
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-01-21 22:11:31 -0500

    Time: 15 ms (42.65%) | Memory: 19.6 MB (12.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,22 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        # if lengths don't match, return False
         if len(s) != len(t):
             return False
+        else:
+            # create a dict which maps letter -> frequency for s
+            s_dict = dict()
+            for x in s:
+                s_dict[x] = s_dict.get(x, 0) + 1
             
-        letters = dict()
-
-        for ch in s:
-            letters[ch] = letters.get(ch, 0) + 1
-        
-        for ch in t:
-            if ch in letters.keys():
-                if letters[ch] == 1:
-                    del letters[ch]
+            # then traverse t and use the same dictionary to check
+            for y in t:
+                if y in s_dict.keys():
+                    s_dict[y] -= 1
                 else:
-                    letters[ch] -= 1
-            else:
-                return False
-        
-        if len(letters) == 0:
+                    return False
+            
+            for _, v in s_dict.items():
+                if v != 0:
+                    return False
+            
             return True
-        return False
+
+        
