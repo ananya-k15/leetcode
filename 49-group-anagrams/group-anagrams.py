bf7514318f0d9b2340232606868c68c002d924db
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-03 14:44:10 -0500

    Time: 19 ms (23.82%) | Memory: 24.1 MB (16.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,31 +1,44 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        # create a dictionary to store 
-        # anagram_info : list of words
-        # anagram_info = tuple with letters in word in asc order + their frequencies
+        # edge case to consider = empty strings!
+        # we need to map each string in strs to a key that stores 
+        # which letters are in that string + their frequency
+        # if we do this for each string, we'll end up mapping 
+        # all the anagrams of a string to the same "key"
+        # "aet" could be a key for ate, eat and tea.
+        # but strings can be 100 letters long, this would mean big keys.
+        # alternative: use a fixed length key, like a dictionary that maps
+        # letter : freq (there can only be 26 letters!)
+        # or a list/tuple of length 26, where freq of a is stored at index 0, 
+        # freq of a is stored at index 1, and so on. 
+
+        # create an empty hashmap
         anagrams = dict()
 
-        # for each word in strs
+        # loop over all words in strs
         for word in strs:
-            if len(word) == 0:
-                anagrams[""] = anagrams.get("", []) + [""]
+            # Create a special index for empty strings
+            if word == "":
+                anagrams[0] = anagrams.get(0, []) + [""]
                 continue
 
-            # compute anagram info into a sorted tuple
-            letters = dict()
-            for l in word:
-                letters[l] = letters.get(l, 0) + 1
-            key = tuple(sorted(letters.items()))
+            # for each word, initialize a list of length 26 for frequencies
+            freq = [0 for x in range(0,26,1)]
+        
+            # traverse each word, and for each letter
+            for letter in word:
+                # the index is ascii value of letter - 65 
+                # since a is at the 97th position
+                # add +1 to the frequencies list
+                freq[ord(letter)-97] += 1
 
-            # check if it already exists in dict
-            # if yes, update list value to include this word
-            # if no, add entry with this word
-            if key in anagrams:
-                anagrams[key].append(word)
-            else:
-                anagrams[key] = [word]
+            # check if anagrams aleady has this key
+            anagram_key = tuple(freq) # turn to a tuple since dictionary keys must be immutable
+            # if so, add this word to the key's value
+            # if not, add a new entry to anagrams
+            anagram_value = anagrams.get(anagram_key, [])
+            anagrams[anagram_key] = anagram_value + [word]
 
-        # loop through dict to create a list of lists
-        # or return all the values of the anagrams dict
-        return list(anagrams.values())
+        # extract all the values from anagrams (list of lists)
+        return [x[1] for x in anagrams.items()]
