9b76be65844be83365552f0ce69e22da26552a09
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-03 19:20:47 -0500

    Time: 3 ms (76.34%) | Memory: 22.4 MB (17.33%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,11 +7,12 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # to store diameter value
-        self.diameter = 0
+        maxDiameter = 0
         
         # returns height of tree, and computes diameter in the process
         def height(root: Optional[TreeNode]) -> int:
-            
+            nonlocal maxDiameter
+
             # edgecase: empty tree
             # since we automatically count each subtree, 
             # we use -1 to correct that assumption when the tree does not exist
@@ -27,9 +28,9 @@ class Solution:
 
             # diameter formed by passing through this root node
             diameter = lh + rh + 2
-            self.diameter = max(self.diameter, diameter)
+            maxDiameter = max(maxDiameter, diameter)
 
             return current_height
-            
+
         height(root)
-        return self.diameter
+        return maxDiameter
