286a1e435866ddd3f7d8f18eb42cfc4e624cf511
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-03 19:46:58 -0500

    Time: 0 ms (100.00%) | Memory: 20.3 MB (88.99%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -23,8 +23,12 @@ class Solution:
             lh = dfsHeight(root.left)
             rh = dfsHeight(root.right)
 
+            if lh == -1 or rh == -1 :
+                return -1
+
             if abs(lh - rh) > 1:
                 balanced = False
+                return -1
 
             return 1 + max(lh, rh)
 
