148d84905351cda2e002368b81f1643828fcf804
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-03 23:34:17 -0500

    Time: 63 ms (65.41%) | Memory: 22.8 MB (61.09%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,18 +7,31 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # We need to compute min(p, q) and max(p, q)
-        pq_min = min(p.val, q.val)
-        pq_max = max(p.val, q.val)
 
-        # if pq_min > root.value, look in the right subtree
-        if pq_min > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
+        # create a variable to hold the current node
+        current = root
 
-        # if pq_max < root.value, look in the left subtree
-        if pq_max < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        # if the node exists
+        while current is not None:
+            if p.val > current.val and q.val > current.val:
+                current = current.right
+            elif p.val < current.val and q.val < current.val:
+                current = current.left
+            else:
+                return current
 
-        # otherwise, pq_min <= root.value <= pq_max
-        # the root is the LCA
-        return root
+        # # We need to compute min(p, q) and max(p, q)
+        # pq_min = min(p.val, q.val)
+        # pq_max = max(p.val, q.val)
+
+        # # if pq_min > root.value, look in the right subtree
+        # if pq_min > root.val:
+        #     return self.lowestCommonAncestor(root.right, p, q)
+
+        # # if pq_max < root.value, look in the left subtree
+        # if pq_max < root.val:
+        #     return self.lowestCommonAncestor(root.left, p, q)
+
+        # # otherwise, pq_min <= root.value <= pq_max
+        # # the root is the LCA
+        # return root
