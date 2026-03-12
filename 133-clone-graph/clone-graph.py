823582d9778eb17f566f29d5a20803e20bc41ac6
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-12 18:08:12 -0400

    Time: 44 ms (88.28%) | Memory: 19.8 MB (83.19%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,48 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # edgecase: empty graph
+        if node is None:
+            return None
+
+        # create a seenNodes hashmap to track node val : newNode
+        seenNodes = dict()
+
+        # helper function: cloneNode
+        def cloneNode(node):
+            # print(node.val, node.neighbors)
+            # for every node in org graph p
+            # check if we've already cloned p
+            if node.val in seenNodes.keys():
+                # if yes, return seenNodes[p]
+                return seenNodes[node.val]
+            # if no, clone p and add it to the seenNodes hashmap
+            newNode = Node(node.val, [])
+            seenNodes[node.val] = newNode
+            # print(newNode, seenNodes)
+            # for all neighbours of p
+            if node.neighbors is not None:
+                for n in node.neighbors:
+                    # call cloneNode and assign the result to the list
+                    m = cloneNode(n)
+                    # print("cloned node", m.val, m.neighbors, newNode.neighbors)
+                    temp = newNode.neighbors
+                    # return cloned node
+                    if temp is None:
+                        newNode.neighbors = [m]
+                    newNode.neighbors = temp + [m]
+            return newNode
+
+        # call cloneNode on the first node
+        # return the deep copy 
+        return cloneNode(node)
+
+        
