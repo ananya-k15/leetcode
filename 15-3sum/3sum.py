2fe71c5c1a64133d331d2d35448b12af16817684
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-17 15:43:53 -0400

    Time: 510 ms (83.87%) | Memory: 22.2 MB (71.76%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,12 +1,16 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         
-        results = set()
+        results = []
 
         # sort the list to get the non decreasing order of elements
         nums.sort()
 
         for i in range(len(nums)):
+            # Skip duplicate values for i
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
             # to get to target, we need to find j and k s.t.
             # nums[k] + nums[j] == -nums[i]
             # if nums[i] < 0, look on the right hand side
@@ -17,13 +21,22 @@ class Solution:
             
             j, k = i+1, len(nums)-1
             while j < k:
-                if nums[j] + nums[k] + nums[i] == 0:
-                    results.add((nums[i], nums[j], nums[k]))
+                total = nums[j] + nums[k] + nums[i]
+                if total == 0:
+                    results.append([nums[i], nums[j], nums[k]])
+
+                    # Skip duplicates for j
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    # Skip duplicates for k
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+
                     j += 1
                     k -= 1
-                elif nums[j] + nums[k] + nums[i] > 0:
+                elif total > 0:
                     k -= 1
                 else:
                     j += 1
-        res = [list(x) for x in results]
-        return res
+        # res = [list(x) for x in results]
+        return results
