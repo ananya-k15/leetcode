966f31ce79638a7df2e9ac7d128754d62d8373d0
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-17 15:40:38 -0400

    Time: 1735 ms (6.71%) | Memory: 23.7 MB (8.00%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        
+        results = set()
+
+        # sort the list to get the non decreasing order of elements
+        nums.sort()
+
+        for i in range(len(nums)):
+            # to get to target, we need to find j and k s.t.
+            # nums[k] + nums[j] == -nums[i]
+            # if nums[i] < 0, look on the right hand side
+            # if nums[i] > 0, all the remaining numbers are positive 
+            # so we can skip this (we would have already found a possible combo)
+            if nums[i] > 0:
+                break
+            
+            j, k = i+1, len(nums)-1
+            while j < k and k < len(nums):
+                if nums[j] + nums[k] + nums[i] == 0:
+                    results.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif nums[j] + nums[k] + nums[i] > 0:
+                    k -= 1
+                else:
+                    j += 1
+        res = [list(x) for x in results]
+        return res
