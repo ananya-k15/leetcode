985bd6e6e31adea57894ebb45af2b322d1e8ec02
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-18 18:57:13 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.08%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,19 +1,26 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-                
+        # let's call the index where the array was rotated the kink
+        # we're trying to find the kink, i.e., the smallest value
+        # in this case, we perform binary search to explicitly look
+        # at the unsorted part of the array. 
+        # If the entire sub array is sorted, return the first index of the left side.
+        
         # set our left and right indices
         l, r = 0, len(nums)-1
 
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
 
-            if nums[l] < nums[mid] and nums[mid] < nums[r]:
+            if nums[l] <= nums[mid] and nums[mid] <= nums[r]:
                 return nums[l]
-            elif nums[l] < nums[mid]:
+            # left side is sorted, look in the right side
+            elif nums[l] <= nums[mid]:
                 l = mid + 1
-            elif nums[mid] < nums[r]:
+            # right side is sorted, look in the left side
+            elif nums[mid] <= nums[r]:
                 r = mid
             else:
-                l = mid + 1
+                print("what could this be", l, r, mid)
         
         return nums[l]
