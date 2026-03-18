ca530e458f1d1dfceae5d3b31f7e400977b5d288
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-18 14:03:19 -0400

    Time: 90 ms (73.31%) | Memory: 28.4 MB (69.96%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -6,15 +6,13 @@ class Solution:
         results = [0] * t # default value is 0
         # traverse the temperatures array using index i
         for i in range(t):
-            current = temperatures[i]
             # at each step, while the stack is non empty
             # and top of stack < current element
-            while stack != [] and stack[-1][0] < current:
+            while stack and temperatures[stack[-1]] < temperatures[i]:
                 # pop top of stack, get it's index j
-                j = stack[-1][1]
-                stack.pop()
+                j = stack.pop()
                 # add the diff i-j to the jth position in the results array
                 results[j] = i-j
             # once loop ends, add current element, i 
-            stack.append([current, i])
+            stack.append(i)
         return results
