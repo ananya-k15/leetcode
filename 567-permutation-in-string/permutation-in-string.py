b536f7fd6be8722e01e1e62a275cfd45c01b39e1
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-18 09:37:02 -0400

    Time: 46 ms (29.46%) | Memory: 19.4 MB (46.06%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -7,9 +7,15 @@ class Solution:
             return False
         # create a counter to map char -> freq in s2
         s1count = Counter(s1)
-        for x in range(0, n-k+1, 1):
-            s2count = Counter(s2[x:x+k])
+        s2count = Counter(s2[:k])
+        if s1count == s2count:
+            return True
+        for r in range(k, n):
+            # s2count = Counter(s2[x:x+k])
+            s2count[s2[r]] += 1
+            s2count[s2[r-k]] -= 1
+            if s2count[s2[r-k]] == 0:
+                del s2count[s2[r-k]]
             if s1count == s2count:
                 return True
-
         return False
