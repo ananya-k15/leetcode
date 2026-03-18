a92dc20bf089c7adce86496f933f280da868b98a
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-03-18 09:23:59 -0400

    Time: 99 ms (45.20%) | Memory: 19.7 MB (77.60%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,14 +7,14 @@ class Solution:
         l = 0
         maxSubLen = 0
         subString = Counter()
+        maxFreq = 0
 
         for r in range(len(s)):
             # if condition is not met, shrink window from left
             subString[s[r]] += 1
-            maxFreq = subString.most_common(1)[0][1]
-            subLen = r - l + 1
+            maxFreq = max(maxFreq, subString[s[r]])
             # print("At (", l, ", ", r, "subLen", subLen, "maxFreq", maxFreq, "and k", k)
-            if subLen - maxFreq > k:
+            if (r - l + 1) - maxFreq > k:
                 subString[s[l]] -= 1
                 l += 1
             # print(r - l + 1, maxSubLen)
