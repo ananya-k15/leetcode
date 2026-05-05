a3eacb9e64074aa7b9ebbd13f3a52442655b6a82
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-05 00:49:58 -0400

    Time: 8 ms (7.44%) | Memory: 27.8 MB (45.66%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,23 +8,44 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Use slow and fast pointers to find the midpoint
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        mid = slow.next
-        slow.next = None
-        # Reverse the second half of the list 
-        prev, curr = None, mid
-        while curr:
+
+        if head.next is None:
+            return head
+
+        # Find the end of the list using slow/fast pointers
+        mid, end = head, head
+        while end.next and end.next.next:
+            mid = mid.next
+            end = end.next.next
+
+        # Sever the two lists
+        head2 = mid.next
+        mid.next = None
+
+        # Reverse the second half of the linked list
+        prev, curr = head2, head2.next
+        prev.next = None
+        while curr is not None:
             temp = curr.next
             curr.next = prev
             prev, curr = curr, temp
-        head2 = prev
-        # Then merge the two lists until both halves are exhausted
-        while head2:
-            temp1, temp2 = head.next, head2.next
-            head.next = head2
-            head2.next = temp1
-            head, head2 = temp1, temp2
+
+        list2 = prev
+        list1 = head
+
+        # Merge the two lists
+        dummy = mlist = ListNode()
+        flip = 1
+        while list1 and list2:
+            if flip == 1:
+                mlist.next = list1
+                list1 = list1.next
+                flip = 0
+            else:
+                mlist.next = list2
+                list2 = list2.next
+                flip = 1
+            mlist = mlist.next
+        mlist.next = list1 or list2
+
+        return dummy.next
