1babf3ac97cdf8534ca9fe8f71629c013cf8beff
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-06 06:20:10 -0400

    Time: 5 ms (31.52%) | Memory: 22.3 MB (16.85%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -12,17 +12,15 @@ class Solution:
             nonlocal maxDiameter
             if node is None:
                 return -1
-
             # at each node, we have two options:
             # either the maxDiameter passes through the current node
             # or the maxDiameter is the height of a subtree, depending on:
             # height(left) + height(right) + 2 > height(left/right) + 1
-
             lh, rh = height(node.left), height(node.right)
-            diameter = max(lh + rh + 2, lh + 1, rh + 1)
-            if diameter > maxDiameter:
-                maxDiameter = diameter
-            return max(lh, rh) + 1
+            currentHeight = max(lh, rh) + 1
+            diameter = lh + rh + 2
+            maxDiameter = max(maxDiameter, diameter)
+            return currentHeight
 
         height(root)
         return maxDiameter
