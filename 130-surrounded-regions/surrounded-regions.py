760dec2ba1d58f2fd9056f7876473a8bca030842
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-07 06:08:21 -0400

    Time: 7 ms (53.43%) | Memory: 22.7 MB (19.38%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,32 +3,34 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # find all non surrounded regions
-        # start exploring from the edges and create a set for all such nodes
-        # finally, traverse the board and mark everything not in the set as 'X'
-        surrounded = set()
+        
         m, n = len(board), len(board[0])
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        neighbours = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visited = set()
 
-        def explore(x, y):
-            nonlocal surrounded
-            # check for invalid indices
-            # if board[x][y] == "O":
-            surrounded.add((x, y))
-            for dx, dy in dirs:
-                nx, ny = x+dx, y+dy
-                if (0 <= nx < m) and (0 <= ny < n) and (nx, ny) not in surrounded and board[nx][ny] == "O":
-                    explore(nx, ny)
-    
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0 or i == m-1 or j == n-1:
-                    if (i, j) not in surrounded and board[i][j] == "O":
-                        explore(i, j)
+        def surround(x, y):
+            nonlocal board, visited
+            if x >= 0 and x < m and y >= 0 and y < n and (x, y) not in visited and board[x][y] == "O":
+                visited.add((x, y))
+                for dx, dy in neighbours:
+                    surround(x + dx, y + dy)
 
-        for i in range(m):
-            for j in range(n):
-                if (i, j) not in surrounded:
-                    board[i][j] = "X"
+        # check for any islands touching the edges
+        for i in range(0, m):
+            if board[i][0] == "O":
+                surround(i, 0)
+            if board[i][n-1] == "O":
+                surround(i, n-1)
+        for j in range(0, n):
+            if board[0][j] == "O":
+                surround(0, j)
+            if board[m-1][j] == "O":
+                surround(m-1, j)
 
+        for u in range(0, m):
+            for v in range(0, n):
+                if (u, v) not in visited:
+                    board[u][v] = "X"
+
+        
         
