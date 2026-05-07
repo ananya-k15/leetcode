13d15b89a11fad839f1e50ae13c5e52325c41877
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-07 03:01:59 -0400

    Time: 267 ms (21.81%) | Memory: 31.3 MB (7.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        # while exploring
-        def explore(x, y):
-            nonlocal seen, m, n, grid
-            # check north, south, east and west only
-            # if we hit an seen 1 or 0 -> return
-            # if we hit invalid indices -> return
-            if (x >= 0 and x < m) and (y >= 0 and y < n) and ((x, y) not in seen) and grid[x][y] == "1":
-                grid[x][y] = "0"
-                # if we hit an unseen 1 -> recursively explore that 1
-                explore(x+1,y)
-                explore(x,y+1)
-                explore(x-1,y)
-                explore(x,y-1)
-            return
-
-            
-        islandCount = 0
-        seen = set()
         m, n = len(grid), len(grid[0])
+        neighbours = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visited = set()
+        
+        def dfs(x, y):
+            nonlocal grid
+            if x >= 0 and x < m and y >= 0 and y < n:
+                if (x, y) not in visited and grid[x][y] == "1":
+                    visited.add((x, y))
+                    grid[x][y] = "0"
+                    for dx, dy in neighbours:
+                        dfs(x + dx, y + dy)
 
-        # we traverse the grid to find a 1
-        for i in range(0, m, 1):
-            for j in range(0, n, 1):
-                # print(seen)
-                if grid[i][j] == "0":
-                    continue
-                # once we hit a one -> we've got an island
-                else:
-                    # append islandCount
-                    islandCount += 1
-                    # explore all neighbours to check island area
-                    # add all 1's discovered this way to a set of seen 1's
-                    explore(i, j)
-    
-        return islandCount
+        islands = 0
+
+        for i in range(0, m):
+            for j in range(0, n):
+                if grid[i][j] == "1":
+                    islands += 1
+                    dfs(i, j)
+        
+        return islands
+                
+                
+            
