0cadb30230294f57f37911013e9f122c3820d2cf
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-07 03:02:51 -0400

    Time: 244 ms (58.61%) | Memory: 31.5 MB (5.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,24 +3,18 @@ class Solution:
         m, n = len(grid), len(grid[0])
         neighbours = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         visited = set()
-        
         def dfs(x, y):
-            nonlocal grid
-            if x >= 0 and x < m and y >= 0 and y < n:
-                if (x, y) not in visited and grid[x][y] == "1":
-                    visited.add((x, y))
-                    grid[x][y] = "0"
-                    for dx, dy in neighbours:
-                        dfs(x + dx, y + dy)
-
+            if x >= 0 and x < m and y >= 0 and y < n and (x, y) not in visited and grid[x][y] == "1":
+                visited.add((x, y))
+                grid[x][y] = "0"
+                for dx, dy in neighbours:
+                    dfs(x + dx, y + dy)
         islands = 0
-
         for i in range(0, m):
             for j in range(0, n):
                 if grid[i][j] == "1":
                     islands += 1
                     dfs(i, j)
-        
         return islands
                 
                 
