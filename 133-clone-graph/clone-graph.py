4f2e8b6e8066da5d413a5e2ce7c8e0470b259782
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-07 05:51:12 -0400

    Time: 41 ms (94.71%) | Memory: 19.7 MB (55.72%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,35 +9,24 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # edgecase: empty graph
         if node is None:
             return None
-
-        # create a seenNodes hashmap to track node val : newNode
-        seenNodes = dict()
-
-        # helper function: cloneNode
-        def cloneNode(node):
-            # for every node in org graph p
-            # check if we've already cloned p
-            if node.val in seenNodes.keys():
-                # if yes, return seenNodes[p]
-                return seenNodes[node.val]
-            # if no, clone p and add it to the seenNodes hashmap
-            newNode = Node(node.val, [])
-            seenNodes[node.val] = newNode
-            # for all neighbours of p
-            if node.neighbors is not None:
-                for n in node.neighbors:
-                    # call cloneNode and assign the result to the list
-                    m = cloneNode(n)
-                    # if newNode.neighbors is None:
-                    #     newNode.neighbors = [m]
-                    newNode.neighbors = newNode.neighbors + [m]
-            return newNode
-
-        # call cloneNode on the first node
-        # return the deep copy 
-        return cloneNode(node)
-
         
+        stack = [node]
+        seen = {}
+
+        while stack != []:
+            n = stack.pop()
+            if n not in seen:
+                seen[n] = Node(n.val)
+            for x in n.neighbors:
+                if x not in seen:
+                    stack.append(x)
+                    xCopy = Node(x.val, [])
+                    seen[x] = xCopy
+                seen[n].neighbors.append(seen[x])
+
+        return seen[node]
+
+                    
+
