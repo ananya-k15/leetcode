e7d2679da334443c42479158adb1a6fdfd7c9e82
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-07 07:19:50 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.55%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -7,10 +7,14 @@ class Solution:
         queue = deque([])
         maxTime = 0
 
-        def rot():
-            nonlocal grid, visited, queue, maxTime
-            if len(queue) == 0:
-                return 
+        # traverse the entire grid, looking for rotten oranges
+        for i in range(0, m):
+            for j in range(0, n):
+                if grid[i][j] == 2:
+                    queue.append((i, j, 0))
+        
+
+        while len(queue) != 0:
             x, y, time = queue.popleft()
             if (x, y) not in visited and grid[x][y] == 2:
                 visited.add((x, y))
@@ -20,17 +24,6 @@ class Solution:
                         if (x + dx, y + dy) not in visited and grid[x+dx][y+dy] == 1:
                             grid[x+dx][y+dy] = 2
                             queue.append((x + dx, y + dy, time+1))
-            rot()
-
-        # traverse the entire grid, looking for rotten oranges
-        for i in range(0, m):
-            for j in range(0, n):
-                if grid[i][j] == 2:
-                    queue.append((i, j, 0))
-                    # begin rotting the oranges at time=0
-                    # for di, dj in neighbours:
-                    #     queue.append((i + di, j + dj, 1))
-        rot()
 
         # if there is a fresh orange left, return -1
         for i in range(0, m):
