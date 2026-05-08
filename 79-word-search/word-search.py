e44da1544181b2a6ee6962c55b2cba702c31ef85
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-08 01:27:07 -0400

    Time: 3045 ms (88.97%) | Memory: 19.4 MB (57.58%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,39 +2,37 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
         neighbours = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        visited = set()
-        def backtrack(x, y, current):
-            # print(current, x, y, word)
+        def backtrack(x, y, idx):
             # base case = return True if current matches word
-            if current == word:
-                # print("returning true")
+            if idx == len(word):
                 return True
             # for all possible vertical and horizontal neighbours
             for dx, dy in neighbours:
                 # prune if indices are invalid
                 nx, ny = x + dx, y + dy
-                # print(nx, ny, visited)
                 if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                    continue
-                if (nx, ny) in visited:
                     continue
                 # prune if current's letter does not match the 
                 # corresponding letter in word
-                if board[nx][ny] != word[len(current)]:
+                if board[nx][ny] != word[idx]:
                     continue
-                visited.add((nx, ny))
+                temp = board[nx][ny]
+                board[nx][ny] = 0
                 # otherwise, backtrack
-                if backtrack(nx, ny, current + board[nx][ny]):
+                if backtrack(nx, ny, idx + 1):
                     return True
                 # remove this letter and explore other neighbours
-                visited.remove((nx, ny))
+                board[nx][ny] = temp
             # return False if no backtracking possibility worked
             return False
+
         for i in range(0, m):
             for j in range(0, n):
                 if board[i][j] == word[0]:
-                    visited.add((i, j))
-                    if backtrack(i, j, word[0]):
+                    temp = board[i][j]
+                    board[i][j] = 0
+                    if backtrack(i, j, 1):
                         return True
-                    visited.remove((i, j))
+                    board[i][j] = temp
+
         return False
