91abd0b35ac2c61f541449d683900f3c3f28c45f
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-08 00:35:52 -0400

    Time: 2 ms (37.68%) | Memory: 19.6 MB (9.33%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -2,9 +2,9 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
         n = len(nums)
+        visited = set()
         # we use backtracking
         def backtrack(current):
-            nonlocal results
             # base case to add to results: length should be 3
             if len(current) == n:
                 results.append(current[:])
@@ -13,13 +13,15 @@ class Solution:
             # for all values in nums
             for ind in range(0, n):
                 # prune if there are duplicates
-                if nums[ind] in current:
+                if ind in visited:
                     continue
                 # add the value to current
+                visited.add(ind)
                 current.append(nums[ind])                
                 # recursively call backtrack on this value
                 backtrack(current)
                 # remove value from current and proceed
                 current.pop()
+                visited.remove(ind)
         backtrack([])
         return results
