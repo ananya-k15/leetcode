ce0d10af41ce6f21f16ec408982b4c82ee519c49
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-11 10:08:23 -0400

    Time: 142 ms (47.21%) | Memory: 20.1 MB (44.25%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -5,7 +5,7 @@ class Solution:
         # for each value val in nums, we want to find 
         # findTargetSumWays(target - val) and findTargetSumWays(target - val) right?
         def helper(ind, target, memo):
-            key = str(target) + "," + str(ind)
+            key = (target, ind)
             if key in memo:
                 return memo[key]
             if ind < 0:
