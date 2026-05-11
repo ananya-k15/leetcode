ae8f259613d838ca55de07809187b588a89acc82
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2026-05-11 08:17:25 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.73%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -1,10 +1,13 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         factor = []
+        heapq.heapify(factor)
         for x in range(1, n+1):
             if n % x == 0:
-                factor.append(x)
+                heapq.heappush(factor, -x)
+                if len(factor) > k:
+                    heapq.heappop(factor)
 
         if len(factor) < k:
             return -1
-        return factor[k-1]
+        return -1 * factor[0]
